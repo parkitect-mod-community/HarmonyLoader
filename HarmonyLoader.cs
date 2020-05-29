2ee5a6d48f7d9833daef02c8e0097300c0bc8e6a
Ryan Mytton <43795565+Topodic@users.noreply.github.com>
2020-05-29 11:31:44 -0700

    Initial commit

---
+++ HarmonyLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+
+namespace HarmonyLoader
+{
+    public class HarmonyLoader : IMod
+    {
+        public string Name => "Harmony Library Loader";
+        public string Description => "Loads Harmony, so your mods don't have to (enable first!)";
+        public string Identifier => "Harmony@ParkitectMods";
+
+        private static AppDomain appDomain;
+        private static Assembly harmonyAssembly;
+        private static string executingDirectory;
+
+        public void onDisabled()
+        {
+        }
+
+        public void onEnabled()
+        {
+            appDomain = AppDomain.CurrentDomain;
+
+            var assemblyList = appDomain.GetAssemblies();
+
+            if (Array.Exists<Assembly>(assemblyList, match: element => element.GetName().Name == "0Harmony"))
+            {
+                FileLog.Log("Harmony already present in app domain."); //Harmony's logger
+                return;
+            }
+
+            executingDirectory = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            harmonyAssembly = Assembly.LoadFrom(executingDirectory + "\\0Harmony.dll");
+            appDomain.Load(harmonyAssembly.GetName());
+
+            if (Array.Exists<Assembly>(assemblyList, match: element => element.GetName().Name == "0Harmony"))
+            {
+                FileLog.Log("Harmony successfully loaded into app domain.");
+            }
+        }
+    }
+}
---
+++ HarmonyLoaderTester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+
+namespace HarmonyLoaderTester
+{
+    public class HarmonyLoaderTester : IMod
+    {
+        public string Name => "Harmony Loaded Tester";
+
+        public string Description => "Tests if Harmony is loaded";
+
+        public string Identifier => "HarmonyLoaderTester@Topodic";
+
+        public void onDisabled()
+        {
+        }
+
+        public void onEnabled()
+        {
+            Assembly[] currentAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            if (Array.Exists<Assembly>(currentAssemblies, match: element => element.GetName().Name == "0Harmony"))
+            {
+                FileLog.Log("Harmony is loaded.");
+            }
+        }
+    }
+}
